f5516b8171fb38e4a4d7287772daf674f84a5d62
stampFabian <fabian.trenkwalder@hak-kitz.ac.at>
2023-05-02 12:18:33 +0200

    initial commit part 2

--- app/src/main/java/com/example/intenttest/MainActivity.java
+++ app/src/main/java/com/example/intenttest/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.intenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button StartButton = findViewById(R.id.btn_start);
+
+        StartButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/intenttest/NewsActivity.java
+++ app/src/main/java/com/example/intenttest/NewsActivity.java
@@ -1,5 +1,4 @@
 package com.example.intenttest;
 
 public class NewsActivity {
-
 }
