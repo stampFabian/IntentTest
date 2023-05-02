86d8fe2d45009849aead4a29343af75c0498fbd3
stampFabian <fabian.trenkwalder@hak-kitz.ac.at>
2023-05-02 12:47:23 +0200

    initial commit part 3

--- app/src/main/java/com/example/intenttest/NewsActivity.java
+++ app/src/main/java/com/example/intenttest/NewsActivity.java
@@ -1,4 +1,22 @@
 package com.example.intenttest;
 
-public class NewsActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+
+        Button useless_button = findViewById(R.id.button);
+
+        useless_button.setOnClickListener(v -> {
+            Intent intent = new Intent(NewsActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
 }
